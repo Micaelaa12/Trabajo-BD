76085d66d340f053eaf9d7508db5075c238dc18e
Micaelaa12 <90358115+Micaelaa12@users.noreply.github.com>
2023-11-15 20:37:19 -0300

    script sql

---
+++ trabajo bd - municoy.sql
@@ -0,0 +1,20 @@
+use integrador_cac; 
+
+insert into oradores (nombre, apellido, mail, tema) values 
+("Martina", "Gonzalez", "martigonza@gmail.com", "Tecnología"),
+("Juan", "Maruiz", "juanma@gmail.com", "Arte"), 
+("Miqueas", "Cabello", "mikecab@gmail.com", "Marketing"),
+("Carina", "Romero", "cariromero@gmail.com", "Redes Sociales"),
+("Facundo", "Lopez", "faculopez@gmail.com", "Inteligencia Artificial"),
+("Marta", "Pereyra", "martap@gmail.com", "Psicología"),
+("Claudio", "Acosta", "clauacosta@gmail.com", "Historia"),
+("Valeria", "Gimenez", "valgimenez@gmail.com", "Diseño"),
+("Camilo", "Haag", "camihaag@gmail.com", "Cibernetica"),
+("Antonella", "Galez", "antogalez@gmail.com", "Economía");
+
+select * from oradores;
+
+describe oradores; 
+
+
+	
